818a9044b704c1c22b6803988f276d36a7120fbc
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-06-11 16:44:36 -0700

    Update LandingScreen.js

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -2,26 +2,33 @@ import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button } from "react-native-elements";
 import NavLink from "../components/NavLink";
+import { FontAwesome5 } from "@expo/vector-icons";
 const LandingScreen = ({ navigation }) => {
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          See what's happening in the world right now.
-        </Text>
-        <Button
-          buttonStyle={styles.button}
-          title="Create Account"
-          onPress={() => navigation.navigate("Signup")}
-        />
-
-        <NavLink
-          style={styles.navlink}
-          routeName="Signin"
-          text="Have an account already? Log in"
+    <View style={styles.container}>
+      <View>
+        <FontAwesome5
+          style={{ alignSelf: "center" }}
+          name="paw"
+          size={24}
+          color="#1DA1F2"
         />
       </View>
-    </>
+      <Text style={styles.text}>
+        See what's happening in the world right now.
+      </Text>
+      <Button
+        buttonStyle={styles.button}
+        title="Create Account"
+        onPress={() => navigation.navigate("Signup")}
+      />
+
+      <NavLink
+        style={styles.navlink}
+        routeName="Signin"
+        text="Have an account already? Log in"
+      />
+    </View>
   );
 };
 LandingScreen.navigationOptions = () => {
@@ -44,6 +51,8 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     color: "black",
   },
-  navlink: {},
+  navlink: {
+    marginTop: 20,
+  },
 });
 export default LandingScreen;
