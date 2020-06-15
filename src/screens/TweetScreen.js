2469eb1834c0063a9fcc29621167ee6d4766c946
Bahbi Lee <superbahbi@users.noreply.github.com>
2020-06-15 16:57:40 -0700

    Added header and card

--- src/screens/TweetScreen.js
+++ src/screens/TweetScreen.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Text, Button } from "react-native-elements";
-import { SafeAreaView } from "react-navigation";
-import { Feather } from "@expo/vector-icons";
+import tweetApi from "../api/tweetApi";
+import { View, StyleSheet, Image } from "react-native";
+import {
+  Text,
+  Card,
+  ListItem,
+  Button,
+  Icon,
+  Header,
+} from "react-native-elements";
+import { Feather, FontAwesome5 } from "@expo/vector-icons";
+
 const TweetScreen = () => {
   return (
-    <SafeAreaView forceInset={{ top: "always" }}>
+    <>
+      <Header
+        barStyle="light-content"
+        leftComponent={<Feather name="circle" size={20} color="#1DA1F2" />}
+        centerComponent={<FontAwesome5 name="paw" size={20} color="#1DA1F2" />}
+        rightComponent={<Feather name="star" size={20} color="#1DA1F2" />}
+        containerStyle={{
+          backgroundColor: "#fff",
+          justifyContent: "space-around",
+          height: 50,
+          paddingTop: 0,
+        }}
+      />
       <Text>Tweet</Text>
-    </SafeAreaView>
+      <Card containerStyle={styles.card}>
+        <ListItem key={0} roundAvatar title="bahbi" avatar="none" />
+      </Card>
+    </>
   );
 };
 TweetScreen.navigationOptions = {
+  headerShown: true,
   tabBarOptions: {
     showLabel: false,
   },
@@ -18,5 +42,9 @@ TweetScreen.navigationOptions = {
 };
 const styles = StyleSheet.create({
   container: {},
+  card: {
+    padding: 0,
+    margin: 0,
+  },
 });
 export default TweetScreen;
